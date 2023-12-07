3af30b96b22a63988e48d22cf3749893f97bd049
Tobias Zimmermann <info@tzdesign.de>
2023-12-07 14:55:48 +0100

    Initial Commit

---
+++ SwiftUIPageViewController/SwiftUIPageViewControllerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIPageViewControllerApp.swift
+//  SwiftUIPageViewController
+//
+//  Created by Tobias Zimmermann on 07.12.23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIPageViewControllerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
